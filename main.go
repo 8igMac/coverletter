8ee4a009e190ec7bed714e7a5b50c4a051945933
8igmac <8igmac@pm.me>
2023-06-03 15:36:43 +0800

    [FEAT] Generate cover letter based on template.

--- main.go
+++ main.go
@@ -1,7 +1,82 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/go-pdf/fpdf"
+)
+
+func combine(template string, company string, position string, team string) string {
+	template = strings.Replace(template, "{position}", position, -1)
+	template = strings.Replace(template, "{team}", team, -1)
+	template = strings.Replace(template, "{company}", company, -1)
+	return template
+}
+
+func get_template(template_filename string) string {
+	// Read template from file.
+	file, err := os.Open(template_filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var template string
+	for scanner.Scan() {
+		template += scanner.Text() + "\n"
+	}
+
+	return template
+}
+
+func output_pdf(content string, filename string) error {
+	log.Println("Outputing cover letter...")
+	pdf := fpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "", 12)
+
+	page_width, _ := pdf.GetPageSize()
+	_, _, r, _ := pdf.GetMargins()
+	width := page_width - 2*r
+	height := 5.0
+	pdf.MultiCell(width, height, content, "", "", false)
+	err := pdf.OutputFileAndClose(filename)
+	return err
+}
 
 func main() {
-	fmt.Println("Generate cover letter for you.")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Enter the position name: ")
+	scanner.Scan()
+	position := scanner.Text()
+
+	fmt.Print("Enter the team name: ")
+	scanner.Scan()
+	team := scanner.Text()
+
+	fmt.Print("Enter the company name: ")
+	scanner.Scan()
+	company := scanner.Text()
+
+	fmt.Print("Enter the template file name: ")
+	scanner.Scan()
+	template_filename := scanner.Text()
+
+	template := get_template(template_filename)
+	content := combine(template, company, position, team)
+
+	fmt.Print("Enter the output file name: ")
+	scanner.Scan()
+	filename := scanner.Text()
+
+	err := output_pdf(content, filename)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
